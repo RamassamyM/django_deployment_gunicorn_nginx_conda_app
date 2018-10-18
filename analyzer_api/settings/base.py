e52aeac4e63dfb64cad5ad8037ad8eb685ba4b91
michael.ramassamy <michael.ramassamy@poietis.com>
2018-10-18 11:01:45 +0200

    Comment dbug toolbar and reset configuration

--- analyzer_api/settings/base.py
+++ analyzer_api/settings/base.py
@@ -106,7 +106,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
